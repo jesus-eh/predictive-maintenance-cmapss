e43a1f557325e8d400db806b72e3667a3b67cb01
jesus-eh <chechuescano981@gmail.com>
2025-08-20 13:06:29 +0200

    Modificaciones

--- src/model.py
+++ src/model.py
@@ -1,4 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+
 
 def train_rf(X_train, y_train, n_estimators: int = 300, random_state: int = 42):
     model = RandomForestRegressor(n_estimators=n_estimators, random_state=random_state, n_jobs=-1)
@@ -7,7 +9,6 @@ def train_rf(X_train, y_train, n_estimators: int = 300, random_state: int = 42):
 
 
 def train_xgb(X_train, y_train):
-    from xgboost import XGBRegressor
     model = XGBRegressor(n_estimators=300, learning_rate=0.05, max_depth=6, subsample=0.8, colsample_bytree=0.8, random_state=42)
     model.fit(X_train, y_train)
     return model
